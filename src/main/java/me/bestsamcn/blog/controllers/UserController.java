1c665c7d0a135ba5a0a26d65029c2aebcad427ed
bestsamcn <645298225@qq.com>
2018-10-27 23:24:20 +0800

    ss”

--- src/main/java/me/bestsamcn/blog/controllers/UserController.java
+++ src/main/java/me/bestsamcn/blog/controllers/UserController.java
@@ -1,8 +1,28 @@
 package me.bestsamcn.blog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Author: Sam
  * @Date: 2018/10/26 0:35
  */
+@Controller
 public class UserController {
+    @ResponseBody
+    @RequestMapping("/json")
+    public Map<String, String> json(){
+        Map<String, String> map = new HashMap();
+        map.put("name", "123");
+        return map;
+    }
+    @RequestMapping("/")
+    public String index(){
+        return "/test";
+    }
+
 }
