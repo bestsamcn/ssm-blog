d5cc1976487e26fbcbe9d4076c96d195bb383b92
bestsamcn <645298225@qq.com>
2018-11-14 23:47:35 +0800

    enum

--- src/main/java/me/bestsamcn/blog/controllers/ArticleController.java
+++ src/main/java/me/bestsamcn/blog/controllers/ArticleController.java
@@ -1,9 +1,12 @@
 package me.bestsamcn.blog.controllers;
 
+import me.bestsamcn.blog.models.Article;
+import me.bestsamcn.blog.models.Notify;
 import me.bestsamcn.blog.services.ArticleService;
 import me.bestsamcn.blog.utils.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,5 +27,19 @@ public class ArticleController {
     public Response add(){
         Response res = articleService.add();
         return res;
+    }
+
+    @GetMapping(name="查询", value="getById")
+    @ResponseBody
+    public Response getById(){
+        try{
+            Article article = articleService.selectById("24fe562b9a794727afbbfbd1d2d09b6d");
+            if(article != null){
+                return Response.build(article);
+            }
+            return Response.error("无此数据");
+        }catch (Exception e){
+            return Response.error();
+        }
     }
 }
